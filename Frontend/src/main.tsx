2cdae71980e08097cc3a461617854b7a52417438
Danish-Dsouza <daanish.dsouza@gmail.com>
2025-04-27 11:49:53 +0530

    Full project reset: cleanup and re-initialize
    - Removed legacy files and components
    - Re-initialized git repository and branch
    - Updated .gitignore and README
    - Updated Storybook and Vite configuration files
    - Cleaned up old assets and source files
    - Added new Storybook setup and public assets

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,13 +1,10 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@/styles/global.css";
 import "./index.css";
-import { BrowserRouter } from "react-router";
-import { AppRouter } from "./routes/AppRouter";
-import { AuthProvider } from "@/contexts/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
-  </BrowserRouter>
+  <StrictMode>
+    <div></div>
+  </StrictMode>
 );
--- Frontend/src/pages/Onboarding.tsx
+++
@@ -1,6 +0,0 @@
-export default function Onboarding() {
-  console.log("hello");
-  return (
-    <div className="h-screen w-screen bg-black text-white">onboarding</div>
-  );
-}
